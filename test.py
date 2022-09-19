6d46a9136d473970fc6a7185d018ad7b67022dc7
Yikang Ding <dyk20@mails.tsinghua.edu.cn>
2022-09-20 00:22:29 +0800

    update the default parameters of gipuma fusion

--- test.py
+++ test.py
@@ -47,9 +47,9 @@ parser.add_argument('--conf', type=float, default=0.03, help='prob confidence')
 parser.add_argument('--thres_view', type=int, default=5, help='threshold of num view')
 #filter by gimupa
 parser.add_argument('--fusibile_exe_path', type=str, default='../fusibile/fusibile')
-parser.add_argument('--prob_threshold', type=float, default='0.05')
+parser.add_argument('--prob_threshold', type=float, default='0.01')
 parser.add_argument('--disp_threshold', type=float, default='0.25')
-parser.add_argument('--num_consistent', type=float, default='5')
+parser.add_argument('--num_consistent', type=float, default='3')
 # parse arguments and check
 args = parser.parse_args()
 print("argv:", sys.argv[1:])
