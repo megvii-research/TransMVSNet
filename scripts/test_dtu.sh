fb347af955a1292a896ebb2a8b8559ed8e383096
Yikang Ding <dyk20@mails.tsinghua.edu.cn>
2022-09-23 14:20:30 +0800

    Update test_dtu.sh

--- scripts/test_dtu.sh
+++ scripts/test_dtu.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 TESTPATH="/data/DTU/dtu-test" 						# path to dataset dtu_test
 TESTLIST="lists/dtu/test.txt"
-CKPT_FILE="checkpoints/model_dtu.ckpt" # path to checkpoint file
-FUSIBLE_PATH="" 													# path to fusible of gipuma
+CKPT_FILE="checkpoints/model_dtu.ckpt"			   # path to checkpoint file, you need to use the model_dtu.ckpt for testing
+FUSIBLE_PATH="" 								 	# path to fusible of gipuma
 OUTDIR="outputs/dtu_testing" 						  # path to output
 if [ ! -d $OUTDIR ]; then
 	mkdir -p $OUTDIR
@@ -20,6 +20,7 @@ python test.py \
 --ndepths="48,32,8" \
 --depth_inter_r="4.0,1.0,0.5" \
 --interval_scale=1.06 \
---filter_method="normal"
-#--filter_method="gipuma" \
-#--fusibile_exe_path=$FUSIBLE_PATH
+--filter_method="gipuma" \
+--fusibile_exe_path=$FUSIBLE_PATH
+#--filter_method="normal"
+
