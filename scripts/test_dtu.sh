a40525385fb084cc36efe7724a17323515e91126
Hariz Izzat <harizizzat09@gmail.com>
2023-02-06 20:13:12 +0800

    cleanup script

--- scripts/test_dtu.sh
+++ scripts/test_dtu.sh
@@ -10,17 +10,17 @@ fi
 
 
 python test.py \
---dataset=general_eval \
---batch_size=1 \
---testpath=$TESTPATH  \
---testlist=$TESTLIST \
---loadckpt=$CKPT_FILE \
---outdir=$OUTDIR \
---numdepth=192 \
---ndepths="48,32,8" \
---depth_inter_r="4.0,1.0,0.5" \
---interval_scale=1.06 \
---filter_method="gipuma" \
---fusibile_exe_path=$FUSIBLE_PATH
-#--filter_method="normal"
+	--dataset=general_eval \
+	--batch_size=1 \
+	--testpath=$TESTPATH  \
+	--testlist=$TESTLIST \
+	--loadckpt=$CKPT_FILE \
+	--outdir=$OUTDIR \
+	--numdepth=192 \
+	--ndepths="48,32,8" \
+	--depth_inter_r="4.0,1.0,0.5" \
+	--interval_scale=1.06 \
+	--filter_method="gipuma" \
+	--fusibile_exe_path=$FUSIBLE_PATH | tee -a $LOG_DIR/log.txt
+	#--filter_method="normal"
 
