c01a5b14adc5b77533e95c4a401fd8063300054d
Hariz Izzat <harizizzat09@gmail.com>
2023-02-06 20:18:53 +0800

    clean-up

--- scripts/test_dtu.sh
+++ scripts/test_dtu.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-TESTPATH="/data/DTU/dtu-test" 						# path to dataset dtu_test
-TESTLIST="lists/dtu/test.txt"
-CKPT_FILE="checkpoints/model_dtu.ckpt"			   # path to checkpoint file, you need to use the model_dtu.ckpt for testing
-FUSIBLE_PATH="" 								 	# path to fusible of gipuma
-OUTDIR="outputs/dtu_testing" 						  # path to output
+TESTPATH="/data/DTU/dtu-test" # path to dataset dtu_test
+TESTLIST="lists/dtu/test.txt" 
+CKPT_FILE="checkpoints/model_dtu.ckpt" # path to checkpoint file, you need to use the model_dtu.ckpt for testing
+FUSIBLE_PATH="fusibile/build/Release/fusibile.exe" # path to fusible of gipuma
+OUTDIR="outputs/dtu_testing" # path to output
 if [ ! -d $OUTDIR ]; then
 	mkdir -p $OUTDIR
 fi
